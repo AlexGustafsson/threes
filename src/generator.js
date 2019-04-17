50b54db790eebc4f279c3cdfc5b3432465017529
Alex Gustafsson <alex-gustafsson@live.se>
2019-04-17 21:15:34 +0200

    Generate grid that fits

--- src/generator.js
+++ src/generator.js
@@ -16,19 +16,33 @@ async function generate(username, options) {
   const ctx = canvas.getContext('2d');
   const random = new Random(username);
 
-  drawFullSquare(ctx, random, {
-    left: random.float() * options.width,
-    top: random.float() * options.height,
-    width: 50,
-    height: 50
-  });
+  const wantedWidth = 50;
+  const wantedHeight = 50;
 
-  drawComplementarySquare(ctx, random, {
-    left: random.float() * options.width,
-    top: random.float() * options.height,
-    width: 50,
-    height: 50
-  });
+  const columns = Math.floor(options.width / wantedWidth);
+  const rows = Math.floor(options.height / wantedHeight);
+
+  const width = options.width / columns;
+  const height = options.height / rows;
+
+  for (let y = 0; y < rows; y++) {
+    for (let x = 0; x < columns; x++) {
+      const shapeOptions = {
+        left: x * width,
+        top: y * height,
+        width,
+        height
+      };
+
+      const value = random.float();
+      if (value < 0.3)
+        drawFullSquare(ctx, random, shapeOptions);
+      else if (value < 0.6)
+        drawComplementarySquare(ctx, random, shapeOptions);
+      else
+        drawStripedTriangle(ctx, random, shapeOptions);
+    }
+  }
 
   return canvas;
 }
@@ -57,6 +71,30 @@ function drawComplementarySquare(ctx, random, {left, top, width, height}) {
   ctx.lineTo(left + width, top);
   ctx.lineTo(left + width, top + height);
   ctx.fill();
+}
+
+function drawStripedTriangle(ctx, random, {left, top, height}) {
+  const color = random.color();
+
+  const stripeHeight = 5;
+  const stripeSpace = stripeHeight;
+  let stripes = 0;
+  for (; (stripes * stripeHeight) + ((stripes - 1) * stripeSpace) < height; stripes++);
+
+  for (let i = 0; i < stripes; i++) {
+    const topLeft = {x: 0, y: (i * (stripeHeight + stripeSpace))};
+    const topRight = {x: topLeft.y, y: topLeft.y};
+    const bottomLeft = {x: 0, y: topLeft.y + stripeHeight};
+    const bottomRight = {x: bottomLeft.y, y: bottomLeft.y};
+
+    ctx.fillStyle = color;
+    ctx.beginPath();
+    ctx.moveTo(topLeft.x + left, topLeft.y + top);
+    ctx.lineTo(topRight.x + left, topRight.y + top);
+    ctx.lineTo(bottomRight.x + left, bottomRight.y + top);
+    ctx.lineTo(bottomLeft.x + left, bottomLeft.y + top);
+    ctx.fill();
+  }
 }
 
 module.exports = {
