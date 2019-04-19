9f6b443d507fb3b6220083aac87e16f410a878c3
Alex Gustafsson <alex-gustafsson@live.se>
2019-04-19 16:15:23 +0200

    Implemented GitHub style

--- src/generators/github.js
+++ src/generators/github.js
@@ -1,9 +1,48 @@
+const RELATIVE_MARGIN = 0.09;
+const COLUMNS = 5;
+const ROWS = 5;
+const MIN_DRAW_CHANCE = 0.4;
+const MAX_DRAW_CHANCE = 0.75;
+
 function generate(username, ctx, random, options) {
-  const margin = 0.09;
+  const margin = {
+    horizontal: RELATIVE_MARGIN * options.width,
+    vertical: RELATIVE_MARGIN * options.height
+  };
+
+  const cellSize = {
+    width: (options.width - (2 * margin.horizontal)) / COLUMNS,
+    height: (options.height - (2 * margin.vertical)) / COLUMNS
+  };
 
   const backgroundColor = '#F0F0F0';
   ctx.fillStyle = backgroundColor;
   ctx.fillRect(0, 0, options.width, options.height);
+
+  const foregroundColor = random.color();
+  const drawChance = random.float();
+  ctx.fillStyle = foregroundColor;
+  for (let y = 0; y < ROWS; y++) {
+    for (let x = 0; x < Math.ceil(COLUMNS / 2); x++) {
+      if (random.float() > Math.max(Math.min(MAX_DRAW_CHANCE, drawChance), MIN_DRAW_CHANCE))
+        continue;
+
+      const position = {
+        x: margin.horizontal + (x * cellSize.width),
+        y: margin.vertical + (y * cellSize.height)
+      };
+
+      const mirroredPosition = {
+        x: options.width - (2 * margin.horizontal) - position.x,
+        y: position.y
+      };
+
+      ctx.fillRect(position.x, position.y, cellSize.width, cellSize.height);
+
+      if (x < Math.floor(COLUMNS / 2))
+        ctx.fillRect(mirroredPosition.x, mirroredPosition.y, cellSize.width, cellSize.height);
+    }
+  }
 }
 
 module.exports = generate;
